1e821c73331a521ae5335e4dc0593f8db5732c4a
ionb23 <ion.borziac@gmail.com>
2023-01-17 22:41:12 +0000

    final clean up

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -20,23 +20,3 @@ var questions = [
 		correctAnswer: 'Document Object Model'
 	},
 ];
-
-
-// var questions = {
-//     question1: {
-//         question: 'What is W?',
-//         choices: ['123', '456', '789', '999']
-//     },
-//     question2: {
-//         question: 'What is X?',
-//         choices: ['123', '456', '789', '999']
-//     },
-//     question3: {
-//         question: 'What is Y?',
-//         choices: ['123', '456', '789', '999']
-//     },
-//     question4: {
-//         question: 'What is Z?',
-//         choices: ['123', '456', '789', '999']
-//     }
-// };
